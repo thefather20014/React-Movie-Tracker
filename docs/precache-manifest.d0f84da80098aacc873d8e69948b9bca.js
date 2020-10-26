30add2324d12506d254a854e49b5da8be2f5829a
Carlos Aracena <carlosthefather2014@hotmail.com>
2020-10-26 18:09:40 -0400

    Second Commit

---
+++ docs/precache-manifest.d0f84da80098aacc873d8e69948b9bca.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6f88f7d12c864115a2b31db92cff990",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a5cc0a2b47cdb2f5fe2",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "dd2f66429638849d6e6a",
+    "url": "/static/css/main.9acfd471.chunk.css"
+  },
+  {
+    "revision": "8a5cc0a2b47cdb2f5fe2",
+    "url": "/static/js/2.3bef8036.chunk.js"
+  },
+  {
+    "revision": "306f3bf9d2f1ee8399ad560a7731d0d6",
+    "url": "/static/js/2.3bef8036.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd2f66429638849d6e6a",
+    "url": "/static/js/main.82a678a4.chunk.js"
+  },
+  {
+    "revision": "1bd9e98fe862317cc4c2",
+    "url": "/static/js/runtime-main.6fba0f5b.js"
+  },
+  {
+    "revision": "0516dc05ac47988bae3f1c52b0f1d609",
+    "url": "/static/media/404.0516dc05.jpg"
+  },
+  {
+    "revision": "75605cba82b63771b753c26e58ee81ef",
+    "url": "/static/media/logo.75605cba.gif"
+  },
+  {
+    "revision": "24a6fdb93a3bb0275107bfc71637cb21",
+    "url": "/static/media/movieLogo.24a6fdb9.svg"
+  },
+  {
+    "revision": "54002c09d004500300d90aa9163ee6a8",
+    "url": "/static/media/user.54002c09.svg"
+  }
+]);
